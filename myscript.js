3dcb1baf8cfdca2dad158aeacc4ff7b85f4331ce
Zoteroazi <148597519+Zoteroazi@users.noreply.github.com>
2024-11-10 01:18:45 +0100

    Add Check Winner and Play Round

--- myscript.js
+++ myscript.js
@@ -4,3 +4,33 @@ function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice
 }
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if (
+        (playerSelection === "rock" && computerSelection ==="scissors") ||
+        (playerSelection === "paper" && computerSelection ==="rock") ||
+        (playerSelection === "scissors" && computerSelection ==="paper")
+        
+    ) {return "Player"}
+    else {
+        return "Computer"
+    }
+
+    function playRound(playerSelection, computerselection) {
+        const result = checkWinner(playerSelection, computerSelection)
+
+        if (result == "Tie") {
+            return "It's a Tie"
+        } else if (result == "Player") {
+            return "You won"
+        } else { 
+            return "You lost"
+        }
+    }
+
+    function getPlayerChoice () {
+    }
+    
+}
