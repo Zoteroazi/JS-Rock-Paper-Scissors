877d32753f77ef1c35a4ff2b58e2a6914887f2f5
Zoteroazi <148597519+Zoteroazi@users.noreply.github.com>
2024-11-11 00:47:37 +0100

    Changes to DOM

--- myscript.js
+++ myscript.js
@@ -5,79 +5,54 @@ function getComputerChoice() {
     return choice
 }
 
-function checkWinner(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "Tie";
-    } else if (
-        (playerSelection === "rock" && computerSelection ==="scissors") ||
-        (playerSelection === "paper" && computerSelection ==="rock") ||
-        (playerSelection === "scissors" && computerSelection ==="paper")
-        
-    ) {return "Player"}
-    else {
-        return "Computer"
+//function getPlayerChoice () {
+   //     let validatedInput = false;
+   //     while (validatedInput == false){
+  //          const choice = prompt ("Rock, Paper or Scissors?");
+  //          if (choice == null) {
+  //          continue;
+  //          }
+  //          const choiceInLower = choice.toLowerCase();
+  //          if (options.includes(choiceInLower)) {
+  //             validatedInput = true;
+  //              return choiceInLower;
+  //         }
+  //      }
+  //  }
+
+function game(userChoice) {
+    let playerScore = 0;
+    let computerScore = 0;
+    const computerSelection = getComputerChoice();
+       if (userChoice == computerSelection) {
+        const tie = document.createElement("p");
+        tie.textContent = "Tie";
+        } else if ((userChoice == "rock" && computerSelection == "scissors") ||
+            (userChoice == "paper" && computerSelection == "rock") ||
+            (userChoice == "scissors" && computerSelection == "paper"))
+        {
+            const win = document.createElement("p");
+            win.textContent = "Win";
+        }   else {
+            const lose = document.createElement("p");
+            lose.textContent = "lose";
+        } 
     }
-}
-
-function playRound(playerSelection, computerSelection) {
-        const result = checkWinner(playerSelection, computerSelection)
-
-        if (result == "Tie") {
-            return "It's a Tie"
-        } else if (result == "Player") {
-            return "You won"
-        } else { 
-            return "You lost"
-        }
-    }
-
-function getPlayerChoice () {
-        let validatedInput = false;
-        while (validatedInput == false){
-            const choice = prompt ("Rock, Paper or Scissors?");
-            if (choice == null) {
-            continue;
-            }
-            const choiceInLower = choice.toLowerCase();
-            if (options.includes(choiceInLower)) {
-                validatedInput = true;
-                return choiceInLower;
-            }
-        }
-    }
-
-    function game () {
-        let scorePlayer = 0;
-        let scorePC= 0;
-         for (let i=0; i <5; i++) {
-            const playerSelection = getPlayerChoice();
-            const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection));
-
-            if (checkWinner(playerSelection, computerSelection) == "Player") {
-                scorePlayer++
-            } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-                scorePC++
-            }
-        }
-
-        if (scorePlayer > scorePC)  {
-            console.log("Gamer over, you won!")
-        } else if (scorePlayer < scorePC)  {
-            console.log("Gamer over, Computer won!")
-        } else {
-            console.log("Gamer over, its a tie!")
-
-    }
-
-}
 
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 
 rock.addEventListener("click", function(e) {
-    console.log("penis");
+    game("rock") 
+});
+
+paper.addEventListener("click", function(e) {
+    game("paper")
+});
+
+scissors.addEventListener("click", function(e) {
+    game("scissors")
 });
 
 game ();
