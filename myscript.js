c15b783710575ea8fad6804a74a4ef40e4b735b1
Zoteroazi <148597519+Zoteroazi@users.noreply.github.com>
2024-11-10 20:31:59 +0100

    finish game

--- myscript.js
+++ myscript.js
@@ -18,7 +18,13 @@ function checkWinner(playerSelection, computerSelection) {
         return "Computer"
     }
 
+<<<<<<< HEAD
     function playRound(playerSelection, computerselection) {
+=======
+}
+
+function playRound(playerSelection, computerSelection) {
+>>>>>>> f37e81c (finish game)
         const result = checkWinner(playerSelection, computerSelection)
 
         if (result == "Tie") {
@@ -30,7 +36,52 @@ function checkWinner(playerSelection, computerSelection) {
         }
     }
 
+<<<<<<< HEAD
     function getPlayerChoice () {
     }
     
 }
+=======
+function getPlayerChoice () {
+        let validatedInput = false;
+        while (validatedInput == false){
+            const choice = prompt ("Rock, Paper or Scissors?");
+            if (choice == null) {
+            continue;
+            }
+            const choiceInLower = choice.toLowerCase();
+            if (options.includes(choiceInLower)) {
+                validatedInput = true;
+                return choiceInLower;
+            }
+        }
+    }
+
+    function game () {
+        let scorePlayer = 0;
+        let scorePC= 0;
+        for (let i=0; i <5; i++) {
+            const playerSelection = getPlayerChoice();
+            const computerSelection = getComputerChoice();
+            console.log(playRound(playerSelection, computerSelection));
+
+            if (checkWinner(playerSelection, computerSelection) == "Player") {
+                scorePlayer++
+            } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+                scorePC++
+            }
+        }
+
+        if (scorePlayer > scorePC)  {
+            console.log("Gamer over, you won!")
+        } else if (scorePlayer < scorePC)  {
+            console.log("Gamer over, Computer won!")
+        } else {
+            console.log("Gamer over, its a tie!")
+
+    }
+
+}
+
+game ();
+>>>>>>> f37e81c (finish game)
